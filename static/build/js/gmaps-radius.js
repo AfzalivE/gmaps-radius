baae7f5c16210ed83a433e8678d8a4bb1845ca46
Afzal Najam <afzalnaj@gmail.com>
2015-06-24 02:12:21 -0400

    Search places in clicked circle

--- static/build/js/gmaps-radius.js
+++ static/build/js/gmaps-radius.js
@@ -1,7 +1,8 @@
 (function() {
   $(function() {
-    var circleDrawHandler, circles, earthRadii, map, polygonDestructionHandler, searchBox, searchInput;
+    var activeCircle, activeCircleHandler, circleDrawHandler, circles, createMarker, earthRadii, infoWindow, keywordInput, map, polygonDestructionHandler, radar, radarRequestHandler, radiusInput, searchBox, searchInput, setActiveCircle;
     circles = [];
+    activeCircle = {};
     map = new google.maps.Map($('#map')[0], {
       zoom: 10,
       center: new google.maps.LatLng(51.500358, -0.125506),
@@ -12,6 +13,8 @@
         style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
       }
     });
+    radar = new google.maps.places.PlacesService(map);
+    infoWindow = new google.maps.InfoWindow();
     earthRadii = {
       mi: 3963.1676,
       km: 6378.1,
@@ -25,8 +28,61 @@
       rd: 1268213.63,
       fr: 31705.3408
     };
+    createMarker = function(place) {
+      var marker;
+      marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        icon: {
+          path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z',
+          fillColor: '#ffff00',
+          fillOpacity: 1,
+          scale: 1 / 4,
+          strokeColor: '#bd8d2c',
+          strokeWeight: 1
+        }
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        radar.getDetails(place, function(result, status) {
+          if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            alert(status);
+            return;
+          }
+          infoWindow.setContent(result.name);
+          infoWindow.open(map, marker);
+        });
+      });
+    };
     polygonDestructionHandler = function() {
+      if (activeCircle === this) {
+        activeCircle = {};
+      }
       return this.setMap(null);
+    };
+    radarRequestHandler = function(results, status) {
+      var i, j, place, ref, results1;
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        results1 = [];
+        for (i = j = 0, ref = results.length; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+          place = results[i];
+          results1.push(createMarker(results[i]));
+        }
+        return results1;
+      }
+    };
+    setActiveCircle = function(circle) {
+      if ((activeCircle != null) && Object.keys(activeCircle).length > 0) {
+        activeCircle.setOptions({
+          fillColor: '#004de8'
+        });
+      }
+      activeCircle = circle;
+      return circle.setOptions({
+        fillColor: 'red'
+      });
+    };
+    activeCircleHandler = function(e) {
+      return setActiveCircle(this);
     };
     circleDrawHandler = function(e) {
       var circle, radius, select, unitKey;
@@ -47,8 +103,9 @@
         strokeOpacity: 0.62,
         strokeWeight: 1
       });
+      setActiveCircle(circle);
       google.maps.event.addListener(circle, 'rightclick', polygonDestructionHandler);
-      return google.maps.event.addListener(circle, 'click', circleDrawHandler);
+      return google.maps.event.addListener(circle, 'click', activeCircleHandler);
     };
     google.maps.event.addListener(map, 'click', circleDrawHandler);
     searchInput = document.getElementById('searchInput');
@@ -70,6 +127,33 @@
         } else {
           map.setCenter(location.geometry.location);
         }
+      }
+    });
+    keywordInput = document.getElementById('keywordInput');
+    $(keywordInput.form).on({
+      submit: function() {
+        var keyword, mapLocation, radarRequest, radius;
+        keyword = keywordInput.value;
+        if ((activeCircle != null) && Object.keys(activeCircle).length > 0) {
+          mapLocation = activeCircle.getCenter();
+          radius = activeCircle.getRadius();
+          console.log(mapLocation);
+          console.log(radius);
+          radarRequest = {
+            location: mapLocation,
+            radius: radius,
+            keyword: keyword,
+            rank: google.maps.places.RankBy.DISTANCE
+          };
+          radar.radarSearch(radarRequest, radarRequestHandler);
+        }
+        return false;
+      }
+    });
+    radiusInput = document.getElementById('radiusInput');
+    $(radiusInput.form).on({
+      submit: function() {
+        return false;
       }
     });
     return $(window).on('hashchange', function(e) {
--- static/js/gmaps-radius.coffee
+++ static/js/gmaps-radius.coffee
@@ -1,5 +1,6 @@
 $ ->
     circles = []
+    activeCircle = {}
     
     map = new google.maps.Map($('#map')[0], {
         zoom: 10
@@ -10,6 +11,9 @@ $ ->
         mapTypeControlOptions:
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
     })
+
+    radar = new google.maps.places.PlacesService(map)
+    infoWindow = new google.maps.InfoWindow();
     
     earthRadii = {
         # The radius of the earth in various units
@@ -26,8 +30,49 @@ $ ->
         fr: 31705.3408
     }
     
+    createMarker = (place) ->
+      # console.log  place.geometry.location
+      marker = new (google.maps.Marker)(
+        map: map
+        position: place.geometry.location
+        icon:
+          path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z'
+          fillColor: '#ffff00'
+          fillOpacity: 1
+          scale: 1 / 4
+          strokeColor: '#bd8d2c'
+          strokeWeight: 1)
+      google.maps.event.addListener marker, 'click', ->
+        radar.getDetails place, (result, status) ->
+          if status != google.maps.places.PlacesServiceStatus.OK
+            alert status
+            return
+          infoWindow.setContent result.name
+          infoWindow.open map, marker
+          return
+        return
+      return
+
+
     polygonDestructionHandler = ->
+        if (activeCircle == this)
+            activeCircle = {}
         @setMap(null)
+
+    radarRequestHandler = (results, status) ->
+        if (status == google.maps.places.PlacesServiceStatus.OK)
+            for i in [0 ... results.length]
+                place = results[i]
+                createMarker(results[i])
+
+    setActiveCircle = (circle) ->
+        if (activeCircle? and Object.keys(activeCircle).length > 0) 
+            activeCircle.setOptions(fillColor: '#004de8')
+        activeCircle = circle
+        circle.setOptions(fillColor: 'red')
+    
+    activeCircleHandler = (e) ->
+        setActiveCircle(this)
     
     circleDrawHandler = (e) ->
         # Get the radius in meters (as Google requires)
@@ -49,8 +94,10 @@ $ ->
             strokeOpacity: 0.62
             strokeWeight: 1
         })
+
+        setActiveCircle(circle)
         google.maps.event.addListener(circle, 'rightclick', polygonDestructionHandler)
-        google.maps.event.addListener(circle, 'click', circleDrawHandler)
+        google.maps.event.addListener(circle, 'click', activeCircleHandler)
     
     google.maps.event.addListener(map, 'click', circleDrawHandler)
     
@@ -70,6 +117,29 @@ $ ->
         
         return
     )
+
+    keywordInput = document.getElementById('keywordInput')
+    $(keywordInput.form).on({ submit: -> 
+        keyword = keywordInput.value
+        if (activeCircle? and Object.keys(activeCircle).length > 0) 
+            mapLocation = activeCircle.getCenter()
+            radius = activeCircle.getRadius()
+            console.log mapLocation
+            console.log radius
+
+            radarRequest = {
+                location: mapLocation,
+                radius: radius,
+                keyword: keyword
+                rank: google.maps.places.RankBy.DISTANCE
+            };
+
+            radar.radarSearch(radarRequest, radarRequestHandler);
+        false
+    })
+
+    radiusInput = document.getElementById('radiusInput')
+    $(radiusInput.form).on({ submit : -> false })
     
     $(window).on('hashchange', (e) ->
         query = (new URI()).fragment(true).query(true)
